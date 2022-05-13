dc10ae6e9938e653552ec87b1a0f39866a93609b
Node Sans <node189@icloud.com>
2022-05-13 12:44:23 +0800

    Add incremental selection

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -34,7 +34,15 @@ require 'nvim-treesitter.configs'.setup {
         -- Instead of true it can also be a list of languages
         additional_vim_regex_highlighting = false,
     },
-
+    incremental_selection = {
+        enable = true,
+        keymaps = {
+            init_selection = "<CR>",
+            node_incremental = "<CR>",
+            scope_incremental = "<C-A>",
+            node_decremental = "<BS>",
+        },
+    },
     rainbow = {
         enable = true,
         -- disable = { "jsx", "cpp" }, list of languages you want to disable the plugin for
