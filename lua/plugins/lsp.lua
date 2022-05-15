5512f8f27fdbb3e26621a6cfce17d82001e80aa2
Node Sans <node189@icloud.com>
2022-05-16 00:03:51 +0800

    Remove old config for tabout pluginn

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -58,6 +58,10 @@ require("nvim-lsp-installer").setup({
 })
 
 -- nvim-cmp setup
+local has_words_before = function()
+    local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+    return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+end
 local luasnip = require("luasnip")
 local cmp = require 'cmp'
 cmp.setup {
@@ -91,7 +95,9 @@ cmp.setup {
             if cmp.visible() then
                 cmp.select_next_item()
             elseif luasnip.expand_or_jumpable() then
-                vim.fn.feedkeys(vim.api.nvim_replace_termcodes('<Plug>luasnip-expand-or-jump', true, true, true), '')
+                luasnip.expand_or_jump()
+            elseif has_words_before() then
+                cmp.complete()
             else
                 fallback()
             end
@@ -100,7 +106,7 @@ cmp.setup {
             if cmp.visible() then
                 cmp.select_prev_item()
             elseif luasnip.jumpable(-1) then
-                vim.fn.feedkeys(vim.api.nvim_replace_termcodes('<Plug>luasnip-jump-prev', true, true, true), '')
+                luasnip.jump(-1)
             else
                 fallback()
             end
