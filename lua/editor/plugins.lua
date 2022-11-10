66e745dc70412b18ac8f92c9ba0f7664819ae307
node189 <node189@icloud.com>
2022-11-10 17:53:37 +0800

    Fix lua prompt bug

--- lua/editor/plugins.lua
+++ lua/editor/plugins.lua
@@ -2,7 +2,6 @@ vim.cmd [[packadd packer.nvim]]
 
 local fn = vim.fn
 local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
----@diagnostic disable-next-line: missing-parameter
 if fn.empty(fn.glob(install_path)) > 0 then
     packer_bootstrap = fn.system({ 'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim',
         install_path })
