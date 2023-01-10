17dfc91ac9211cbed42ec0342c7228e77338d218
node189 <node189@icloud.com>
2023-01-10 22:55:06 +0800

    fix(symboloutline.lua): Fix symbol outline

--- lua/plugins/symboloutline.lua
+++ lua/plugins/symboloutline.lua
@@ -1,4 +1,4 @@
-vim.g.symbols_outline = {
+local opts = {
     highlight_hovered_item = false,
     show_guides = false,
     auto_preview = false,
@@ -10,17 +10,6 @@ vim.g.symbols_outline = {
     show_relative_numbers = false,
     show_symbol_details = true,
     preview_bg_highlight = 'Pmenu',
-    keymaps = { -- These keymaps can be a string or a table for multiple keys
-        close = { "<Esc>", "q" },
-        goto_location = "<Cr>",
-        focus_location = "o",
-        hover_symbol = "<C-space>",
-        toggle_preview = "K",
-        rename_symbol = "r",
-        code_actions = "a",
-    },
-    lsp_blacklist = {},
-    symbol_blacklist = {},
     symbols = {
         File = { icon = "", hl = "TSURI" },
         Module = { icon = "", hl = "TSNamespace" },
@@ -45,8 +34,10 @@ vim.g.symbols_outline = {
         Null = { icon = "NULL", hl = "TSType" },
         EnumMember = { icon = "", hl = "TSField" },
         Struct = { icon = "Struct", hl = "TSType" },
-        Event = { icon = "🗲", hl = "TSType" },
+        Event = {icon = "Event", hl = "TSType"},
         Operator = { icon = "+", hl = "TSOperator" },
         TypeParameter = { icon = "𝙏", hl = "TSParameter" }
     }
 }
+
+require("symbols-outline").setup(opts)
